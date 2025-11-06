46d5093970c7349cb99fe657afd4fbc592d32eca
Vipinlal R V <vipinlal.rv@inapp.com>
2025-11-06 10:07:00 +0000

    changes

--- next.config.ts
+++ next.config.ts
@@ -34,7 +34,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
